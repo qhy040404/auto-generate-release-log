38b2842d362f54676fb7724ecd6a1f1639cf4ecc
qhy040404 <45379733+qhy040404@users.noreply.github.com>
2022-08-01 00:33:27 +0800

    Add prompts

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ with open(changelog_path, "r", encoding='utf-8') as changelog:
 
 try:
     proc_log = cl.split(tag_name + fore_delim)[1].split(back_delim)[0]
+    print("Data = " + proc_log)
 except IndexError:
     exit(1)
 
@@ -22,3 +23,5 @@ final_data = final_data.replace(template_data, proc_log)
 
 with open(template_path, "w", encoding='utf-8') as release:
     release.write(final_data)
+
+print("Release body overwritten")
